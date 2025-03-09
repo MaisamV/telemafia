b23a078f772e5cd64932b426ac0b55ac6e5baea0
Maisam <maisam.vahidsafa@gmail.com>
2025-03-09 07:50:31 +0330

    Update help command

--- delivery/telegram/help_handler.go
+++ delivery/telegram/help_handler.go
@@ -9,10 +9,11 @@ func (h *BotHandler) HandleHelp(c telebot.Context) error {
 	help := `Available commands:
 /start - Start the bot
 /help - Show this help message
-/create_room [name] - Create a new room
+/create_room [name] - Create a new room (Admin Only)
 /join_room [room_id] - Join a room
 /leave_room [room_id] - Leave a room
 /list_rooms - List all rooms
-/my_rooms - List your rooms`
+/my_rooms - List your rooms
+/kick_user - Kick a user from a room (Admin Only)`
 	return c.Send(help)
-} 
+}
