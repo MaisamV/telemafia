47fe1365590b5db006d180d18890c729642ecb03
Maisam <maisam.vahidsafa@gmail.com>
2025-03-11 11:40:38 +0330

    Change /start command

--- delivery/telegram/start_handler.go
+++ delivery/telegram/start_handler.go
@@ -1,24 +1,10 @@
 package telegram
 
 import (
-	"fmt"
-	"telemafia/common"
-	userEntity "telemafia/internal/user/entity"
-
 	"gopkg.in/telebot.v3"
 )
 
 // HandleStart handles the /start command
 func (h *BotHandler) HandleStart(c telebot.Context) error {
-	sender := c.Sender()
-	user := userEntity.User{
-		ID:        userEntity.UserID(sender.ID),
-		FirstName: sender.FirstName,
-		LastName:  sender.LastName,
-		Username:  sender.Username,
-		Admin:     common.Contains(h.adminUsernames, c.Sender().Username),
-	}
-
-	c.Send(fmt.Sprintf("%s، به کافه‌مافیا خوش اومدی، سناریو رو انتخاب کن", user.FirstName))
 	return h.HandleListRooms(c)
 }
