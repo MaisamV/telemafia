d2758484f2cb8d880de3c87c002a44d425fe9326
Maisam <maisam.vahidsafa@gmail.com>
2025-03-14 06:10:53 +0330

    Revert start command changes

--- delivery/telegram/start_handler.go
+++ delivery/telegram/start_handler.go
@@ -1,22 +1,10 @@
 package telegram
 
 import (
-	"context"
 	"gopkg.in/telebot.v3"
-	"telemafia/internal/room/usecase/query"
-	userEntity "telemafia/internal/user/entity"
 )
 
 // HandleStart handles the /start command
 func (h *BotHandler) HandleStart(c telebot.Context) error {
-	roomsQuery := query.GetPlayerRoomsQuery{PlayerID: userEntity.UserID(c.Sender().ID)}
-	_, err := h.getPlayerRoomsHandler.Handle(context.Background(), roomsQuery)
-	if err != nil {
-
-	}
-	//if handle != nil && len(handle) > 0 {
-	//	return h.
-	//} else {
 	return h.HandleListRooms(c)
-	//}
 }
